ee1a80a58ce58b3aae05ee43e5518d33269b32e7
MaFa28 <marcomorales.mm41@gmail.com>
2026-07-21 11:07:02 -0400

    usuarios

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // Asegúrate de que la ruta coincida con donde guardaste el archivo
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
